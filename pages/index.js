814d380afd4551b5173f8a0e7152aa83502856cd
Marcelo Figueiredo <marchenrique99@gmail.com>
2022-11-09 11:08:43 -0300

    possivel correção de bugs

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
-    const estilosDaHomePage = {
-        // backgroundColor: "red" 
-    };
+    
     const [valorDoFiltro, setValorDofiltro] = React.useState("");
 
     // console.log(config.playlists);
 
     return (
         <>
-            <CSSReset />
             <div style={{
                 display: "flex",
                 flexDirection: "column",
@@ -44,6 +40,8 @@ export default HomePage
 
 
 const StyledHeader = styled.div`
+    background-color: ${({ theme }) => theme.backgroundLevel1};
+    
     img {
         width: 80px;
         height: 80px;
